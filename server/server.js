c349d79a997fcae0925fa4c030a502e069a8a097
Sascha Ringström <sascha.ringstrom@appliedtechnology.se>
2018-11-23 17:21:36 +0100

    initial commit

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', (_, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
