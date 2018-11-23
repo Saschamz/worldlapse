e6e110fc3a1a22de47638f9779be2718ceba9ac1
Sascha Ringström <sascha.ringstrom@appliedtechnology.se>
2018-11-23 17:37:26 +0100

    fixed untracked files

--- client/src/keys.js
+++
@@ -1,5 +0,0 @@
-
-export default {
-  mapboxToken: 'pk.eyJ1Ijoic2FzY2hhbXoiLCJhIjoiY2pueWE3NGN0MGR0YjNwbzV3Zm5udjFkZiJ9.Yhe8XyJjVRuDsBHs9SsBMw',
-  webcamsToken: 'dEOco1PlZlmshIPqrt4ycn7xZE9zp1Mscwbjsnu53Bztt9kFJ2',
-};
